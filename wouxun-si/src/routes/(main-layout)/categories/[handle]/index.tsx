1a4eff8fb298f7bb91ef0bd6f04981e8ef513369
Primoz Susa <primoz.susa@pm.me>
2024-03-16 00:06:02 +0100

    category

--- wouxun-si/src/routes/(main-layout)/categories/[handle]/index.tsx
+++ wouxun-si/src/routes/(main-layout)/categories/[handle]/index.tsx
@@ -1,81 +1,70 @@
 import { component$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-import type { ProductCategory } from '@medusajs/client-types';
-import { handleError } from '~/modules/logger';
-import { getMedusaClient, getSrvSessionHeaders } from '~/modules/medusa';
+import type { DocumentHead } from '@builder.io/qwik-city';
 import { NotFound } from '~/modules/not-found/NotFound';
+import { SortProducts } from '~/modules/products/SortProducts';
 import { UiText } from '~/ui/UiText';
 import { UiTitle } from '~/ui/UiTitle';
 import { NavLink } from '~/ui/button';
+import {
+  useCategoryByHandle,
+  usePaginatedProductsLoader,
+} from '~/modules/products/loaders';
+import { PaginatedProducts } from '~/modules/products/PaginatedProducts';
 
-export type ProductCategoryWithChildren = Omit<
-  ProductCategory,
-  'category_children'
-> & {
-  category_children: ProductCategory[];
-  category_parent?: ProductCategory;
-};
-
-export const useCategoryByHandle = routeLoader$(async (event) => {
-  const client = getMedusaClient();
-
-  const handle = event.params.handle;
-
-  if (!handle) {
-    event.status(404);
-    return null;
-  }
-
-  const category = await client.productCategories
-    .list(
-      {
-        handle: handle,
-      },
-      getSrvSessionHeaders(event),
-    )
-    .then(({ product_categories: { [0]: category } }) => category)
-    .catch((err) => {
-      handleError(err);
-      return null;
-    });
-  if (!category) event.status(404);
-  return category as ProductCategory | null;
-});
+export {
+  useCategoryByHandle,
+  usePaginatedProductsLoader,
+} from '~/modules/products/loaders';
 
 export default component$(() => {
   const category = useCategoryByHandle();
+  const paginated = usePaginatedProductsLoader();
   return (
     <>
       {!category.value ? (
         <NotFound centered={true} />
       ) : (
-        <div>
-          {category.value.parent_category && (
-            <NavLink
-              href={`/categories/${category.value.parent_category?.handle}`}
-            >
-              {category.value.parent_category?.name}
-            </NavLink>
-          )}
+        <div class="flex flex-col sm:flex-row gap-10">
+          <SortProducts />
+          <div class="w-full">
+            {category.value.parent_category && (
+              <NavLink
+                href={`/categories/${category.value.parent_category?.handle}`}
+              >
+                {category.value.parent_category?.name}
+              </NavLink>
+            )}
 
-          <UiTitle size="xl" as="h1">
-            {category.value.name}
-          </UiTitle>
+            <UiTitle size="xl" as="h1">
+              {category.value.name}
+            </UiTitle>
 
-          {category.value.category_children && (
-            <>
-              <ul class="grid grid-cols-1 gap-2">
-                {category.value.category_children?.map((c) => (
-                  <li key={c.id}>
-                    <NavLink href={`/categories/${c.handle}`}>{c.name}</NavLink>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-          <UiText>{category.value.description}</UiText>
+            {category.value.category_children && (
+              <>
+                <ul class="grid grid-cols-1 gap-2">
+                  {category.value.category_children?.map((c) => (
+                    <li key={c.id}>
+                      <NavLink href={`/categories/${c.handle}`}>
+                        {c.name}
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+            <UiText>{category.value.description}</UiText>
+
+            <PaginatedProducts products={paginated.value.products} />
+          </div>
         </div>
       )}
     </>
   );
 });
+
+export const head: DocumentHead = (event) => {
+  const res = event.resolveValue(useCategoryByHandle);
+  return {
+    title: $localize`${res?.name} category`,
+  };
+};
