3a4e9f340fbe5b355dba8b23db32f9bd079a1588
Primoz Susa <primoz.susa@pm.me>
2024-03-16 18:53:25 +0100

    localize main menu

--- wouxun-si/src/modules/layout/MainNavigation.tsx
+++ wouxun-si/src/modules/layout/MainNavigation.tsx
@@ -1,6 +1,7 @@
 import { component$ } from '@builder.io/qwik';
 import { useContent } from '@builder.io/qwik-city';
 import { NavLink } from '~/ui/button';
+import { useLocale } from '../locale/LocaleProvider';
 
 type Props = {
   darkBg?: boolean;
@@ -9,40 +10,44 @@ type Props = {
 
 export const MainNavigation = component$<Props>((props) => {
   const { menu } = useContent();
+  const locale = useLocale();
+
+  const [localeMenu] =
+    menu?.items?.filter((item) => {
+      return item.text === locale.value;
+    }) ?? [];
 
   return (
     <>
-      {menu
-        ? menu.items?.map((item) => (
-            <ul
-              class={[
-                !props.isMobile &&
-                  `flex justify-center sm:justify-start items-center 
+      {localeMenu?.items?.map((item) => (
+        <ul
+          class={[
+            !props.isMobile &&
+              `flex justify-center sm:justify-start items-center 
                   mr-4
                   space-x-2 whitespace-nowrap sm:space-x-4`,
-                props.isMobile && `space-y-2 whitespace-nowrap text-primary`,
-              ]}
-              key={item.text}
-            >
-              {item.items?.map((item) => {
-                return (
-                  <li key={item.text}>
-                    <NavLink
-                      href={item.href}
-                      color={props.darkBg ? 'neutral' : 'primary'}
-                      size="md"
-                      {...(!props.darkBg && {
-                        activeClass: 'font-semibold text-accent',
-                      })}
-                    >
-                      {item.text}
-                    </NavLink>
-                  </li>
-                );
-              })}
-            </ul>
-          ))
-        : null}
+            props.isMobile && `space-y-2 whitespace-nowrap text-primary`,
+          ]}
+          key={item.text}
+        >
+          {item.items?.map((item) => {
+            return (
+              <li key={item.text}>
+                <NavLink
+                  href={item.href}
+                  color={props.darkBg ? 'neutral' : 'primary'}
+                  size="md"
+                  {...(!props.darkBg && {
+                    activeClass: 'font-semibold text-accent',
+                  })}
+                >
+                  {item.text}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      ))}
     </>
   );
 });
--- wouxun-si/src/routes/menu.md
+++ wouxun-si/src/routes/menu.md
@@ -1,9 +1,21 @@
 # Menu
 
-## Main menu
+## sl
+
+### Glavni meni
 
 - [Ponudba](/product)
 - [Aktualno](/aktualno)
 - [Podpora](/podpora)
 - [Dostava](/info/informacije-o-nakupu-in-dostavi)
 - [Kontakt](/info/kontakt)
+
+## en
+
+### Main menu
+
+- [Products](/product)
+- [Blog](/aktualno)
+- [Support](/podpora)
+- [Delivery](/info/informacije-o-nakupu-in-dostavi)
+- [Contact](/info/kontakt)
