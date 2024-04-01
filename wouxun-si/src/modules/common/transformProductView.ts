ed068dc2a2d27da05c9cd89696fe023cd892b54f
Primoz Susa <primoz.susa@pm.me>
2024-04-01 20:02:59 +0200

    product list with tax

--- wouxun-si/src/modules/common/transformProductView.ts
+++ wouxun-si/src/modules/common/transformProductView.ts
@@ -38,12 +38,12 @@ export const transformProductPreview = (
           calculated_price: formatAmount({
             amount: cheapestVariant.calculated_price,
             region: region,
-            includeTaxes: false,
+            includeTaxes: true,
           }),
           original_price: formatAmount({
             amount: cheapestVariant.original_price,
             region: region,
-            includeTaxes: false,
+            includeTaxes: true,
           }),
           difference: getPercentageDiff(
             cheapestVariant.original_price,
